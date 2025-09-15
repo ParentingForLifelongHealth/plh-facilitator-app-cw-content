a51e08500c7e510e2bf9873838114302b22e09cf
EsmeeIDEMS <esmee@idems.international>
2025-09-15 16:50:44 +0100

    content: 1.2.5

--- config.ts
+++ config.ts
@@ -30,7 +30,7 @@ config.ios.app_name = "";
 
 config.git = {
   content_repo: "https://github.com/ParentingForLifelongHealth/plh-facilitator-app-cw-content.git",
-  content_tag_latest: "1.2.4",
+  content_tag_latest: "1.2.5",
 };
 
 config.firebase = {
