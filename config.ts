380ac0b7eb5b00776fb059455f62221d843c96c1
EsmeeIDEMS <esmee@idems.international>
2025-10-31 14:08:18 +0000

    content: 1.2.35

--- config.ts
+++ config.ts
@@ -32,7 +32,7 @@ config.ios.app_name = "Mayor Konektá";
 
 config.git = {
   content_repo: "https://github.com/ParentingForLifelongHealth/plh-facilitator-app-cw-content.git",
-  content_tag_latest: "1.2.34",
+  content_tag_latest: "1.2.35",
 };
 
 config.firebase = {
