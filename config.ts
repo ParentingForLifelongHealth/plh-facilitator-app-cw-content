4f61a06dff173bd2509232cf2ad62958481bbd36
Chris Marsh <chris.marsh@idems.international>
2025-11-20 15:43:59 +0000

    content: 1.2.52

--- config.ts
+++ config.ts
@@ -32,7 +32,7 @@ config.ios.app_name = "Mayor Konektá";
 
 config.git = {
   content_repo: "https://github.com/ParentingForLifelongHealth/plh-facilitator-app-cw-content.git",
-  content_tag_latest: "1.2.50",
+  content_tag_latest: "1.2.52",
 };
 
 config.firebase = {
